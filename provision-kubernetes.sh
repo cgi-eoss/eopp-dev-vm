3132f5da5518e1689dd49a33f734715267716346
Peter van Zetten <peter.van.zetten@cgi.com>
2021-01-20 17:51:49 +0000

    Update provisioning scripts for idempotency

--- provision-kubernetes.sh
+++ provision-kubernetes.sh
@@ -8,12 +8,12 @@ snap install microk8s --classic
 usermod -a -G microk8s vagrant
 
 # Configure microk8s behind a proxy, if necessary
-if [ ! -z $HTTP_PROXY ] && ! grep -q '^HTTPS_PROXY' /var/snap/microk8s/current/args/containerd-env ; then
+if [ -n "$HTTPS_PROXY" ] && ! grep -q '^HTTPS_PROXY' /var/snap/microk8s/current/args/containerd-env ; then
     cluster_cidr="$(grep 'cluster-cidr' /var/snap/microk8s/current/args/kube-proxy)"
     services_cidr="$(grep 'service-cluster-ip-range' /var/snap/microk8s/current/args/kube-apiserver)"
     containerd_no_proxy="${cluster_cidr#--cluster-cidr=},${services_cidr#--service-cluster-ip-range=}"
     echo -e "HTTPS_PROXY=$HTTPS_PROXY\nNO_PROXY=$containerd_no_proxy" | tee -a /var/snap/microk8s/current/args/containerd-env
-    
+
     microk8s.stop
     microk8s.start
 fi
@@ -40,16 +40,11 @@ if ! microk8s.kubectl -n kube-system get configmap coredns -o yaml | grep -q "fo
 fi
 
 # Set up the `kubectl` and `helm` wrapper functions for users
-echo "" >>/root/.bashrc
-echo -e "kubectl() {\n  microk8s.kubectl \"\$@\"\n}" >>/root/.bashrc
-echo "export -f kubectl" >>/root/.bashrc
-echo -e "helm() {\n  microk8s.helm3 \"\$@\"\n}" >>/root/.bashrc
-echo "export -f helm" >>/root/.bashrc
-echo "" >>/root/.bashrc
-
-echo "" >>/home/vagrant/.bashrc
-echo -e "kubectl() {\n  microk8s.kubectl \"\$@\"\n}" >>/home/vagrant/.bashrc
-echo "export -f kubectl" >>/home/vagrant/.bashrc
-echo -e "helm() {\n  microk8s.helm3 \"\$@\"\n}" >>/home/vagrant/.bashrc
-echo "export -f helm" >>/home/vagrant/.bashrc
-echo "" >>/home/vagrant/.bashrc
+for rcfile in /root/.bashrc /home/vagrant/.bashrc; do
+  if ! grep -q '^kubectl()' "$rcfile"; then
+    printf "\nkubectl() {\n  microk8s.kubectl \"\$@\"\n}\nexport -f kubectl\n" >>"$rcfile"
+  fi
+  if ! grep -q '^helm()' "$rcfile"; then
+    printf "\nhelm() {\n  microk8s.helm3 \"\$@\"\n}\nexport -f helm\n" >>"$rcfile"
+  fi
+done
--- provision-packages.sh
+++ provision-packages.sh
@@ -1,48 +1,72 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-apt-get update
-apt-get -y --no-install-recommends install\
-    apt-transport-https\
-    autoconf\
-    automake\
-    bash-completion\
-    build-essential\
-    curl\
-    firefox\
-    git\
-    git-lfs\
-    gnupg\
-    jq\
-    libdbus-glib-1-2\
-    libgtk-3.0\
-    liblzma-dev\
-    libpng-dev\
-    libxt6\
-    linux-headers-generic\
-    openjdk-11-jdk\
-    openssh-server\
-    python\
-    python-dev\
-    python-setuptools\
-    python-pip-whl\
-    python3\
-    python3-dev\
-    python3-pip\
-    python3-setuptools\
-    python3-wheel\
-    locales\
-    unzip\
-    zip
+function install_missing() {
+  not_installed=()
+  for pkg in "$@"; do
+    if ! dpkg --get-selections "$pkg" 2>&1 | grep -q 'install$'; then
+      not_installed+=("$pkg")
+    fi
+  done
 
-BAZELISK_VER=1.7.4
-NODE_VER=node_12.x
+  if [ -n "${not_installed[*]}" ]; then
+    printf "\n\nInstalling missing required packages: %s\n" "${not_installed[*]}"
+    apt update && apt install -y --no-install-recommends "${not_installed[@]}"
+  else
+    printf "\n\nAll required packages already installed\n"
+  fi
+}
 
-# Set up apt repos
+REQUIRED_PKGS_1=(
+  apt-transport-https
+  autoconf
+  automake
+  bash-completion
+  build-essential
+  curl
+  firefox
+  git
+  git-lfs
+  gnupg
+  jq
+  libdbus-glib-1-2
+  libgtk-3-0
+  liblzma-dev
+  libpng-dev
+  libxt6
+  linux-headers-generic
+  openjdk-11-jdk
+  openssh-server
+  python2
+  python2-dev
+  python-setuptools
+  python-pip-whl
+  python3
+  python3-dev
+  python3-pip
+  python3-setuptools
+  python3-wheel
+  locales
+  unzip
+  zip
+)
+
+REQUIRED_PKGS_2=(
+  bazel
+  nodejs
+  yarn
+)
+
+install_missing "${REQUIRED_PKGS_1[@]}"
+
+BAZELISK_VER="1.7.4"
+NODE_VER="node_12.x"
+
+# Set up additional apt repos
 curl -sL https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | apt-key add -
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" >/etc/apt/sources.list.d/bazel.list
 curl -sL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -56,9 +80,4 @@ curl -sL "https://github.com/bazelbuild/bazelisk/releases/download/v${BAZELISK_V
 chmod +x /usr/local/bin/bazelisk
 ln -snf /usr/local/bin/bazelisk /usr/local/bin/bazel
 
-apt-get update
-apt-get -y --no-install-recommends install\
-    bazel\
-    kubectl\
-    nodejs\
-    yarn
+install_missing "${REQUIRED_PKGS_2[@]}"
